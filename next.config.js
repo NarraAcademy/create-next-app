fa351671fa7d94169201d75d01de94d35cfdd496
Karan <packirisamykaran@gmail.com>
2025-04-08 12:57:07 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 module.exports = {
   reactStrictMode: true,
 };
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/zaid',
+  trailingSlash: true, // optional but helps with static exports sometimes
+};
+
+
+module.exports = nextConfig;
